edd2ec9e5affbedd53d205fed1a3688758a5abdf
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2023-08-07 07:59:29 +0000

    Review comments from @jesserockz accepted.

--- components/kmeteriso/kmeteriso.h
+++ components/kmeteriso/kmeteriso.h
@@ -14,8 +14,8 @@ namespace esphome
         class KMeterISOComponent : public PollingComponent, public i2c::I2CDevice
         {
         public:
-            void set_temperature_sensor(sensor::Sensor *t) { temperature_sensor_ = t; }
-            void set_internal_temperature_sensor(sensor::Sensor *t) { internal_temperature_sensor_ = t; }
+            void set_temperature_sensor(sensor::Sensor *t) { this->temperature_sensor_ = t; }
+            void set_internal_temperature_sensor(sensor::Sensor *t) { this->internal_temperature_sensor_ = t; }
 
             // ========== INTERNAL METHODS ==========
             // (In most use cases you won't need these)
@@ -24,9 +24,6 @@ namespace esphome
             void update() override;
 
         protected:
-            /// Read the temperature value and store the calculated ambient temperature in t_fine.
-            float read_temperature_(const uint8_t *data, int32_t *t_fine);
-
             sensor::Sensor *temperature_sensor_{nullptr};
             sensor::Sensor *internal_temperature_sensor_{nullptr};
             enum ErrorCode
